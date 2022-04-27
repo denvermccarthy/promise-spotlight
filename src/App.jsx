531a6163462d108a5d55cdbc4e6568c67dfab2af
Denver McCarthy <dmichaelspdx@gmail.com>
2022-04-27 09:25:56 -0700

    spotlight work

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import { useEffect } from 'react';
+import { getData, getDataThen, tryThenQuotes } from './services/promise-me';
+
 export default function App() {
+  useEffect(() => {
+    const fetch = () => {
+      const data = thenQuotes();
+      console.log(data);
+    };
+    fetch();
+  }, []);
   return <h1>Hello World</h1>;
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,22 +1,53 @@
 /**
  * TODO: Exercise 1: use `fetch` & async/await to get data from an API
  */
-
+export const getData = async () => {
+  const response = await fetch(`https://futuramaapi.herokuapp.com/api/quotes/1
+  `);
+  const data = response.json();
+  return data;
+};
 /**
  * TODO: Exercise 2: use `fetch` & `.then` syntax to get the same data from the first exercise.
  */
+export const getDataThen = () => {
+  return fetch(`https://futuramaapi.herokuapp.com/api/quotes/1
+    `).then((res) => res.json());
+};
 
 /**
  * TODO: Exercise 3: use `fetch`, async/await, and a try/catch statement to get data from an API and handle errors
  */
+export const tryAsyncQuotes = async () => {
+  try {
+    const data = await getData();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 /**
  * TODO: Exercise 4: use `fetch`, `.then`, and `.catch` to get the same data from exercise 3 while handling errors
  */
 
+export const tryThenQuotes = async () => {
+  return fetch(`https://futuramasapssi.herokuapp.com/api/quotes/1
+    `)
+    .then((res) => res.json())
+    .catch((e) => console.log('something went wrong'));
+};
+
 /**
  * TODO: Exercise 5: use `fetch`, async/await, and a try/catch/finally statement to get data from an API, handle errors, then console.log('All done!') upon completion
  */
+export const tryThenQuotesFinally = async () => {
+  return fetch(`https://futuramaapi.herokuapp.com/api/quotes/1
+    `)
+    .then((res) => res.json())
+    .catch((e) => console.log('something went wrong'))
+    .finally(() => console.log('All done!'));
+};
 
 /**
  * TODO: Exercise 6: use `fetch`, `.then`, `.catch`. and `.finally` to get the same data from exercise 5 while handling errors and calling console.log('All done!') upon completion
@@ -29,4 +60,3 @@
 /**
  * TODO: Exercise 8: Call your function from exercise 6 using async/await
  */
-
